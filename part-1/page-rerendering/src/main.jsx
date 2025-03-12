c78ff7af193a7e3276f4b48a620b4e4f83ecedfd
Mojeed Kusimo <mkusimo90@gmail.com>
2025-03-12 01:52:23 +0100

    feat: Implementing page re-rendering using custom function and state component

---
+++ part-1/page-rerendering/src/main.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+  root.render(
+    <App counter={counter} />
+  )
+}
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+
+setInterval(() => {
+    refresh()
+    counter += 1
+  }, 1000)
